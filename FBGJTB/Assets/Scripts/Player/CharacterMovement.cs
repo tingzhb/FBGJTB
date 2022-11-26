6313a94dcc387d666bcb7fa4a64b6ba24fff3108
Benjamin Ting <tingzhb@gmail.com>
2022-11-26 11:07:31 +0100

    Reformat Character Controller

--- FBGJTB/Assets/Scripts/Player/CharacterMovement.cs
+++ FBGJTB/Assets/Scripts/Player/CharacterMovement.cs
@@ -7,32 +7,45 @@ public class CharacterMovement : MonoBehaviour{
     [SerializeField] private bool isRight;
     private void Update() {
         if (!isRight){
-            if (Input.GetKey(KeyCode.A))
+            if (Input.GetKey(KeyCode.A)){
                 transform.Translate(Vector3.left * (multiplier * Time.deltaTime));
-            if (Input.GetKey(KeyCode.D))
+                Debug.Log("A");
+            }
+            if (Input.GetKey(KeyCode.D)){
                 transform.Translate(Vector3.right * (multiplier * Time.deltaTime));
-            if (Input.GetKey(KeyCode.W))
+            }
+            if (Input.GetKey(KeyCode.W)){
                 transform.Translate(Vector3.forward * (multiplier * Time.deltaTime));
-            if (Input.GetKey(KeyCode.S))
+            }
+            if (Input.GetKey(KeyCode.S)){
                 transform.Translate(Vector3.back * (multiplier * Time.deltaTime));
-            if (Input.GetKey(KeyCode.Q))
+            }
+            if (Input.GetKey(KeyCode.Q)){
                 transform.Rotate(Vector3.down);
-            if (Input.GetKey(KeyCode.E))
+            }
+            if (Input.GetKey(KeyCode.E)){
                 transform.Rotate(Vector3.up);
+            }
         }
         else{
-            if (Input.GetKey(KeyCode.J))
+            if (Input.GetKey(KeyCode.J)){
                 transform.Translate(Vector3.left * (multiplier * Time.deltaTime));
-            if (Input.GetKey(KeyCode.L))
+            }
+            if (Input.GetKey(KeyCode.L)){
                 transform.Translate(Vector3.right * (multiplier * Time.deltaTime));
-            if (Input.GetKey(KeyCode.I))
+            }
+            if (Input.GetKey(KeyCode.I)){
                 transform.Translate(Vector3.forward * (multiplier * Time.deltaTime));
-            if (Input.GetKey(KeyCode.K))
+            }
+            if (Input.GetKey(KeyCode.K)){
                 transform.Translate(Vector3.back * (multiplier * Time.deltaTime));
-            if (Input.GetKey(KeyCode.U))
+            }
+            if (Input.GetKey(KeyCode.U)){
                 transform.Rotate(Vector3.down);
-            if (Input.GetKey(KeyCode.O))
+            }
+            if (Input.GetKey(KeyCode.O)){
                 transform.Rotate(Vector3.up);
+            }
         }
 
     }
