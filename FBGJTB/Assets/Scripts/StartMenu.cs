7c5a9b93b62fd78401549d249bc35891b2708c15
Benjamin Ting <tingzhb@gmail.com>
2022-11-27 11:54:37 +0100

    Sequential Start Menu Added

---
+++ FBGJTB/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour{
+	private bool leftReady, rightReady;
+	[SerializeField] private GameObject leftR, rightR, letsGo;
+	private void Update(){
+		if (Input.GetKey(KeyCode.Space)){
+			leftReady = true;
+			leftR.SetActive(true);
+		}
+		if (Input.GetKey(KeyCode.RightShift)){
+			rightReady = true;
+			rightR.SetActive(true);
+		}
+		if (rightReady && leftReady){
+			leftR.SetActive(false);
+			rightR.SetActive(false);
+			letsGo.SetActive(true);
+			StartCoroutine(DelayStart());
+		}
+	}
+
+	private IEnumerator DelayStart(){
+		yield return new WaitForSeconds(2);
+		SceneManager.LoadScene(sceneBuildIndex: 1);
+	}
+}
