9ba0c1459e6ccef1e9ba349b7346c95dbf885861
Benjamin Ting <tingzhb@gmail.com>
2022-11-27 15:41:34 +0100

    Add Lighting to pickup location

--- FBGJTB/Assets/Scripts/KillZone.cs
+++ FBGJTB/Assets/Scripts/KillZone.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class KillZone : MonoBehaviour
 {
 	private void OnTriggerEnter(Collider other){
-		other.gameObject.GetComponentInParent<PlayerHP>().Damage(3, false);
+		if (other.gameObject.CompareTag("Player")){
+			other.gameObject.GetComponentInParent<PlayerHP>().Damage(3, false);
+		}
 	}
 }
--- FBGJTB/Assets/Scripts/Pickups/Pickups.cs
+++ FBGJTB/Assets/Scripts/Pickups/Pickups.cs
@@ -4,6 +4,7 @@ public class Pickups : MonoBehaviour{
 	[SerializeField] private int pickupNumber;
 	[SerializeField] private float pickupDuration;
 	[SerializeField] private string pickupName;
+	[SerializeField] private GameObject lightning;
 	private void OnTriggerEnter(Collider other){
 		PickupMessage pickupMessage = new(){
 			PickupName = pickupName,
@@ -12,6 +13,9 @@ public class Pickups : MonoBehaviour{
 			PickupPlayerIsRight = other.GetComponentInParent<CharacterMovement>().isRight
 		};
 		Broker.InvokeSubscribers(typeof(PickupMessage), pickupMessage);
+
+		var transform1 = transform;
+		Instantiate(lightning, transform1.position, transform1.rotation);
 		
 		Destroy(gameObject);
 	}
